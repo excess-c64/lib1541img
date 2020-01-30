3094a09a115c09e72e2ea5a71d7b21ac7c98ccd5
Felix Palmen <felix@palmen-it.de>
2020-01-30 17:33:44 +0100

    add conversion utf8 -> petscii

--- include/1541img/petscii.h
+++ include/1541img/petscii.h
@@ -14,9 +14,20 @@
  * This module contains utility functions for processing PETSCII strings.
  */
 
-#include <stdint.h>
+#include <stddef.h>
 
 #include <1541img/decl.h>
+
+/** Handling of case when converting to petscii */
+typedef enum PetsciiCase
+{
+    PC_UPPER	    = 0,		    /**< prefer upper/gfx encoding */
+    PC_LOWER	    = 1,		    /**< prefer lower/upper encoding */
+    PC_FORCE	    = 2,		    /**< only use preferred mode */
+    PC_FORCEUPPER   = PC_UPPER|PC_FORCE,    /**< force upper/gfx encoding */
+    PC_FORCELOWER   = PC_LOWER|PC_FORCE,    /**< force lower/upper encoding */
+    PC_GUESS	    = 4			    /**< try to guess from input */
+} PetsciiCase;
 
 /** Map uppercase graphics chars to lowercase chars.
  * This function modifies a PETSCII string in place, replacing graphics
@@ -26,7 +37,7 @@
  * @param str a pointer to the PETSCII string to modify
  * @param len the length of the PETSCII string to modify
  */
-DECLEXPORT void petscii_mapUpperGfxToLower(char *str, uint8_t len);
+DECLEXPORT void petscii_mapUpperGfxToLower(char *str, size_t len);
 
 /** Map petscii string to UTF-8.
  * Convert a petscii string to a string encoded in UTF-8. The result is
@@ -53,9 +64,34 @@ DECLEXPORT void petscii_mapUpperGfxToLower(char *str, uint8_t len);
  * @returns the number of characters of the resulting UTF-8 string, including
  *     the NUL terminator.
  */
-DECLEXPORT int petscii_toUtf8(
-        char *buf, int bufsz, const char *str, uint8_t len, int lowercase,
+DECLEXPORT size_t petscii_toUtf8(
+        char *buf, size_t bufsz, const char *str, size_t len, int lowercase,
         int onlyPrintable, const char *unknown, const char *shiftspace);
+
+/** Map UTF-8 string to petscii.
+ * Convert a string encoded in UTF-8 to petscii. The result is written to a
+ * buffer provided by the caller, and a NUL terminator is always added. A
+ * buffer the same size as the string to convert (including NUL) is always
+ * sufficient. The function returns the actual buffer size required for the
+ * converted string, so if the return value is larger than the size of the
+ * buffer passed, the result string was truncated.
+ * @param buf pointer to a buffer for the resulting petscii string. If you
+ *     pass NULL here, you have to pass 0 for bufsz as well and the function
+ *     will just return an appropriate buffer size.
+ * @param bufsz size of the result buffer
+ * @param str pointer to the original UTF-8 string
+ * @param len length of the original UTF-8 string
+ * @param casemode how to handle casing during conversion
+ * @param onlyPrintable if set to 1, only printable characters are considered
+ *     for conversion.
+ * @param unknown a single petscii character to use for characters that don't
+ *     exist in petscii. Pass 0 here to just skip them.
+ * @returns the number of characters of the resulting petscii string,
+ *     including the NUL terminator.
+ */
+DECLEXPORT size_t petscii_fromUtf8(
+	char *buf, size_t bufsz, const char *str, size_t len,
+	PetsciiCase casemode, int onlyPrintable, char unknown);
 
 /**@}*/
 
--- src/lib/1541img/petscii.c
+++ src/lib/1541img/petscii.c
@@ -179,9 +179,9 @@ static const char *lcChars[] =
     "\x7a"
 };
 
-SOEXPORT void petscii_mapUpperGfxToLower(char *str, uint8_t len)
+SOEXPORT void petscii_mapUpperGfxToLower(char *str, size_t len)
 {
-    for (uint8_t i = 0; i < len; ++i)
+    for (size_t i = 0; i < len; ++i)
     {
 	if (str[i] == (char)0x62 || str[i] == (char)0xc2)
 	{
@@ -207,6 +207,18 @@ static const char* nonprintable(unsigned char petsciiChar)
     }
 }
 
+static unsigned char nppetscii(char utf8char)
+{
+    switch (utf8char)
+    {
+	case 0x3: return 0x3;
+	case 0x8: return 0x14;
+	case 0xa: return 0xd;
+	case 0xd: return 0x8d;
+	default: return 0;
+    }
+}
+
 static char toScreencode(unsigned char petsciiChar)
 {
     if ((petsciiChar & 0x7f) < 0x20) return -1;
@@ -215,6 +227,14 @@ static char toScreencode(unsigned char petsciiChar)
     if (petsciiChar >= 0x60 && petsciiChar < 0x80) return petsciiChar - 0x20;
     if (petsciiChar >= 0xa0 && petsciiChar < 0xc0) return petsciiChar - 0x40;
     return petsciiChar - 0x80;
+}
+
+static unsigned char toPetsciiChar(char screencode)
+{
+    if (screencode < 0 || screencode > 0x7f) return 0;
+    if (screencode < 0x20 || screencode >= 0x60) return screencode + 0x40;
+    if (screencode >= 0x40) return screencode + 0x20;
+    return screencode;
 }
 
 static const char *lcScreenChar(char screencode)
@@ -236,10 +256,203 @@ static const char *screencodeToUtf8(char screencode, int lowercase)
     return screenChars[(unsigned char)screencode];
 }
 
-static int appendUtf8(char *buf, int bufsz, int *bufpos, const char *chrStr)
+static void guessCase(PetsciiCase *casemode, int lowercase)
 {
-    int chrLen = (int)strlen(chrStr);
-    if (bufsz - *bufpos - 1 > chrLen)
+    if (*casemode & PC_GUESS)
+    {
+	*casemode &= ~PC_GUESS;
+	if (lowercase) *casemode |= PC_LOWER;
+	else *casemode &= ~PC_LOWER;
+    }
+}
+
+#define canMapLower(cm) (!(*(cm) & PC_FORCE) || (*(cm) & PC_LOWER))
+#define canMapUpper(cm) (!(*(cm) & PC_FORCE) || !(*(cm) & PC_LOWER))
+
+static char lowerOrInvalid(unsigned char c, PetsciiCase *casemode)
+{
+    guessCase(casemode, 1);
+    if (canMapLower(casemode)) return c;
+    return 0xff;
+}
+
+static char upperOrInvalid(unsigned char c, PetsciiCase *casemode)
+{
+    guessCase(casemode, 0);
+    if (canMapUpper(casemode)) return c;
+    return 0xff;
+}
+
+static char screencodeFromUtf8(
+	const char *str, size_t strlen, size_t *strpos, PetsciiCase *casemode)
+{
+    if (*strpos >= strlen) return 0xff;
+    unsigned char next = (unsigned char)str[(*strpos)++];
+    if (next < 0x80)
+    {
+	if (next > 0x1f && next < 0x40) return next;
+	if (next > 0x60 && next < 0x7b) 
+	{
+	    return lowerOrInvalid(next - 0x60, casemode);
+	}
+	if (next > 0x40 && next < 0x5b)
+	{
+	    if (*casemode & PC_LOWER) return next;
+	    return next - 0x40;
+	}
+	if (next == 0x40 || next == 0x5b || next == 0x5d) return next - 0x40;
+	return 0xff;
+    }
+    if (*strpos >= strlen) return 0xff;
+    switch (next)
+    {
+	case 0xc2:
+	    next = (unsigned char)str[(*strpos)++];
+	    if (next == 0xa3) return 0x1c;
+	    if (next == 0xa0) return 0x60;
+	    break;
+
+	case 0xe2:
+	    next = (unsigned char)str[(*strpos)++];
+	    if (*strpos >= strlen) return 0xff;
+	    switch (next)
+	    {
+		case 0x86:
+		    next = (unsigned char)str[(*strpos)++];
+		    if (next == 0x91) return 0x1e;
+		    if (next == 0x90) return 0x1f;
+		    break;
+
+		case 0x94:
+		    next = (unsigned char)str[(*strpos)++];
+		    if (next == 0x80) return 0x40;
+		    if (next == 0xbc) return 0x5b;
+		    if (next == 0x82) return 0x5d;
+		    if (next == 0x9c) return 0x6b;
+		    if (next == 0x94) return 0x6d;
+		    if (next == 0x90) return 0x6e;
+		    if (next == 0x8c) return 0x70;
+		    if (next == 0xb4) return 0x71;
+		    if (next == 0xac) return 0x72;
+		    if (next == 0xa4) return 0x73;
+		    if (next == 0x98) return 0x7d;
+		    break;
+
+		case 0x99:
+		    next = (unsigned char)str[(*strpos)++];
+		    if (next == 0xa0) return upperOrInvalid(0x41, casemode);
+		    if (next == 0xa5) return upperOrInvalid(0x53, casemode);
+		    if (next == 0xa3) return upperOrInvalid(0x58, casemode);
+		    if (next == 0xa6) return upperOrInvalid(0x5a, casemode);
+		    break;
+
+		case 0x95:
+		    next = (unsigned char)str[(*strpos)++];
+		    if (next == 0xae) return upperOrInvalid(0x49, casemode);
+		    if (next == 0xb0) return upperOrInvalid(0x4a, casemode);
+		    if (next == 0xaf) return upperOrInvalid(0x4b, casemode);
+		    if (next == 0xb2) return upperOrInvalid(0x4d, casemode);
+		    if (next == 0xb1) return upperOrInvalid(0x4e, casemode);
+		    if (next == 0xad) return upperOrInvalid(0x55, casemode);
+		    if (next == 0xb3) return upperOrInvalid(0x56, casemode);
+		    break;
+
+		case 0x80:
+		    next = (unsigned char)str[(*strpos)++];
+		    if (next == 0xa2) return upperOrInvalid(0x51, casemode);
+		    break;
+
+		case 0x97:
+		    next = (unsigned char)str[(*strpos)++];
+		    if (next == 0x8b) return upperOrInvalid(0x57, casemode);
+		    if (next == 0xa5) return upperOrInvalid(0x5f, casemode);
+		    if (next == 0xa4) return upperOrInvalid(0x69, casemode);
+		    break;
+
+		case 0x96:
+		    next = (unsigned char)str[(*strpos)++];
+		    if (next == 0x8c) return 0x61;
+		    if (next == 0x84) return 0x62;
+		    if (next == 0x94) return 0x63;
+		    if (next == 0x81) return 0x64;
+		    if (next == 0x8e) return 0x65;
+		    if (next == 0x92) return 0x66;
+		    if (next == 0x95) return 0x67;
+		    if (next == 0x97) return 0x6c;
+		    if (next == 0x82) return 0x6f;
+		    if (next == 0x8e) return 0x74;
+		    if (next == 0x8d) return 0x75;
+		    if (next == 0x83) return 0x79;
+		    if (next == 0x96) return 0x7b;
+		    if (next == 0x9d) return 0x7c;
+		    if (next == 0x98) return 0x7e;
+		    if (next == 0x9a) return 0x7f;
+		    break;
+
+		case 0x9c:
+		    next = (unsigned char)str[(*strpos)++];
+		    if (next == 0x93) return lowerOrInvalid(0x7a, casemode);
+		    break;
+	    }
+	    break;
+
+	case 0xf0:
+	    next = (unsigned char)str[(*strpos)++];
+	    if (*strpos >= strlen) return 0xff;
+	    if (next != 0x9f) break;
+	    next = (unsigned char)str[(*strpos)++];
+	    if (*strpos >= strlen) return 0xff;
+	    switch (next)
+	    {
+		case 0xad:
+		    next = (unsigned char)str[(*strpos)++];
+		    if (next == 0xb7) return upperOrInvalid(0x44, casemode);
+		    if (next == 0xb6) return upperOrInvalid(0x45, casemode);
+		    if (next == 0xba) return upperOrInvalid(0x46, casemode);
+		    if (next == 0xb1) return upperOrInvalid(0x47, casemode);
+		    if (next == 0xb4) return upperOrInvalid(0x48, casemode);
+		    if (next == 0xbc) return upperOrInvalid(0x4c, casemode);
+		    if (next == 0xbd) return upperOrInvalid(0x4f, casemode);
+		    if (next == 0xbe) return upperOrInvalid(0x50, casemode);
+		    if (next == 0xbb) return upperOrInvalid(0x52, casemode);
+		    if (next == 0xb0) return upperOrInvalid(0x54, casemode);
+		    if (next == 0xb5) return upperOrInvalid(0x59, casemode);
+		    if (next == 0xbf) return upperOrInvalid(0x7a, casemode);
+		    break;
+
+		case 0xae:
+		    next = (unsigned char)str[(*strpos)++];
+		    if (next == 0x8c) return 0x5c;
+		    if (next == 0x8f) return 0x68;
+		    if (next == 0x87) return 0x6a;
+		    if (next == 0x88) return 0x76;
+		    if (next == 0x82) return 0x77;
+		    if (next == 0x83) return 0x78;
+		    if (next == 0x95) return lowerOrInvalid(0x5e, casemode);
+		    if (next == 0x98) return lowerOrInvalid(0x5f, casemode);
+		    if (next == 0x99) return lowerOrInvalid(0x69, casemode);
+		    break;
+	    }
+	    break;
+
+	case 0xcf:
+	    next = (unsigned char)str[(*strpos)++];
+	    if (*strpos >= strlen) return 0xff;
+	    if (next == 0x80) return upperOrInvalid(0x5e, casemode);
+	    break;
+    }
+    while (*strpos < strlen && ((unsigned char)str[*strpos] & 0xc0) == 0x80)
+    {
+	++(*strpos);
+    }
+    return 0xff;
+}
+
+static size_t appendUtf8(
+	char *buf, size_t bufsz, size_t *bufpos, const char *chrStr)
+{
+    size_t chrLen = strlen(chrStr);
+    if (bufsz > *bufpos + chrLen + 1)
     {
         strcpy(buf + *bufpos, chrStr);
     }
@@ -247,17 +460,18 @@ static int appendUtf8(char *buf, int bufsz, int *bufpos, const char *chrStr)
     return chrLen;
 }
 
-SOEXPORT int petscii_toUtf8(
-        char *buf, int bufsz, const char *str, uint8_t len, int lowercase,
+SOEXPORT size_t petscii_toUtf8(
+        char *buf, size_t bufsz, const char *str, size_t len, int lowercase,
         int onlyPrintable, const char *unknown, const char *shiftspace)
 {
-    int bufpos = 0;
-    for (int i = 0; i < len; ++i)
+    size_t bufpos = 0;
+    size_t result = 0;
+    for (size_t i = 0; i < len; ++i)
     {
         unsigned char petsciiChar = (unsigned char)str[i];
         if (shiftspace && petsciiChar == 0xa0)
         {
-            appendUtf8(buf, bufsz, &bufpos, shiftspace);
+            result += appendUtf8(buf, bufsz, &bufpos, shiftspace);
             continue;
         }
         char screencode = toScreencode(petsciiChar);
@@ -272,12 +486,60 @@ SOEXPORT int petscii_toUtf8(
         }
         if (utf8Char)
         {
-            appendUtf8(buf, bufsz, &bufpos, utf8Char);
+            result += appendUtf8(buf, bufsz, &bufpos, utf8Char);
         }
         else if (unknown)
         {
-            appendUtf8(buf, bufsz, &bufpos, unknown);
+            result += appendUtf8(buf, bufsz, &bufpos, unknown);
         }
     }
-    return bufpos + 1;
+    return result + 1;
 }
+
+static size_t appendPetscii(char *buf, size_t bufsz, size_t *bufpos,
+	unsigned char petscii)
+{
+    if (*bufpos + 1 < bufsz)
+    {
+	buf[(*bufpos)++] = petscii;
+	buf[*bufpos] = 0;
+    }
+    return 1;
+}
+
+SOEXPORT size_t petscii_fromUtf8(
+	char *buf, size_t bufsz, const char *str, size_t len,
+	PetsciiCase casemode, int onlyPrintable, char unknown)
+{
+    size_t strpos = 0;
+    size_t bufpos = 0;
+    size_t result = 0;
+
+    while (strpos < len)
+    {
+	if ((unsigned char)str[strpos] < 0x20)
+	{
+	    if (!onlyPrintable)
+	    {
+		unsigned char npp = nppetscii(str[strpos]);
+		if (npp) result += appendPetscii(buf, bufsz, &bufpos, npp);
+	    }
+	    ++strpos;
+	}
+	else
+	{
+	    char screencode = screencodeFromUtf8(str, len, &strpos, &casemode);
+	    if ((unsigned char)screencode < 0x80)
+	    {
+		result += appendPetscii(
+			buf, bufsz, &bufpos, toPetsciiChar(screencode));
+	    }
+	    else if (unknown)
+	    {
+		result += appendPetscii(buf, bufsz, &bufpos, unknown);
+	    }
+	}
+    }
+    return result + 1;
+}
+
