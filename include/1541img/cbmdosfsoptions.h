302961cd4536d19a4fb6b6df3581eda8217bd85b
Felix Palmen <felix@palmen-it.de>
2020-02-06 17:12:06 +0100

    new flags f. interleave and track alloc strategies

--- include/1541img/cbmdosfsoptions.h
+++ include/1541img/cbmdosfsoptions.h
@@ -12,6 +12,34 @@
  * if both are set. It is invalid to combine CFF_PROLOGICDOSBAM with any of
  * the other extended BAM options, because ProLogic DOS uses a different BAM
  * layout where the disk name is relocated somewhere else.
+ *
+ * CFF_TALLOC_TRACKLOAD and CFF_TALLOC_SIMPLE are different track allocation
+ * strategies that also can't be combined. If none of them is set, the
+ * strategy of the original CBM DOS is used: The first available free sector
+ * is searched for on a track as close as possible to track 18, starting from
+ * track 17 (then 19, then 16, etc). This is a good strategy for random
+ * access to files and prefers the files at the top of the directory.
+ *
+ * CFF_TALLOC_TRACKLOAD is suitable for files that are always read in the same
+ * order, like it is done in many demos -- it will look for the first free
+ * sector starting at track 1.
+ *
+ * CFF_TALLOC_SIMPLE is a compromise between trackload and the original
+ * "nearest track" strategy.
+ *
+ * CFF_TALLOC_PREFDIRTRACK modifies any track allocation strategy by using
+ * track 18 first for files. It only has an effect if CFF_FILESONDIRTRACK is
+ * set as well and is probably a bad idea with the CFF_TALLOC_TRACKLOAD
+ * strategy, because the head would have to seek from track 18 to track 1 in
+ * the middle of loading.
+ *
+ * If CFF_SIMPLEINTERLEAVE is set, interleave values are applied as one
+ * would expect, by adding the interleave and taking the result modulo the
+ * number of sectors.
+ *
+ * Otherwise, interleave is applied in a more complicated way the original
+ * CBM DOS uses: If applying the interleave wraps over sector 0, the result is
+ * decremented by 1.
  */
 typedef enum CbmdosFsFlags
 {
@@ -33,6 +61,17 @@ typedef enum CbmdosFsFlags
     CFF_ZEROFREE = 1 << 7,          /**< report no free blocks in directory */
     CFF_RECOVER = 1 << 8,	    /**< when reading from disk image, try
 				         to recover a broken filesystem */
+    CFF_SIMPLEINTERLEAVE = 1 << 9,  /**< when applying interleave, use a
+                                         simple modulo */
+    CFF_TALLOC_TRACKLOAD = 1 << 10, /**< search for first free sector starting
+                                         from track 1 */
+    CFF_TALLOC_SIMPLE = 1 << 11,    /**< search for first free sector starting
+                                         from track 19, wrapping around to
+                                         track 1, but still don't use tracks
+                                         41 and 42 unless no other tracks have
+                                         free sectors */
+    CFF_TALLOC_PREFDIRTRACK = 1 << 12, /**< if files on dir track are allowed,
+                                         put files there first */
 } CbmdosFsFlags;
 
 /** Filesystem options
